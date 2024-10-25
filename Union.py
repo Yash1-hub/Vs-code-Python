42ea5e84100b2de33f63298277d19bcdf64439c9
Yash1-hub <njsarma2010@gmail.com>
2024-10-25 15:44:23 +0530

    initial commit

---
+++ Union.py
@@ -0,0 +1,6 @@
+Set1 = {'A', 'B', 'C', 'D', 'E'}
+Set2 = {'B', 'D', 'V', 'X', 'Y', 'Z'}
+union = Set1.union(Set2)
+total_guests = list(union)
+print("Total guests to be invited in the Party are", len(total_guests))
+print("Guest List", total_guests)
