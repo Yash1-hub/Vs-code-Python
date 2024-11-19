3713f5d666cefbb45f54b5ddd0c8dc6854347832
Yash1-hub <njsarma2010@gmail.com>
2024-11-19 13:28:11 +0530

    initial commit

---
+++ ball2.py
@@ -0,0 +1,7 @@
+total_balls = 1 + 6 + 3 
+
+white_balls = 3
+
+prob_second_white_given_first_white = white_balls / total_balls
+
+print(f"The probability that the second ball is white given the first is white: {prob_second_white_given_first_white}")
